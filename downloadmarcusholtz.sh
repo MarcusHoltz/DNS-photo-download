5675c0b467bb41bcb553e726836abebf52061ac6
Marcus Holtz <130122354+MarcusHoltz@users.noreply.github.com>
2024-05-09 14:33:36 -0600

    Update downloadmarcusholtz.sh with error handling

--- downloadmarcusholtz.sh
+++ downloadmarcusholtz.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-#
 #####################################################################################
 # Would you like to download a picture of Marcus Holtz? NOW YOU CAN!
 # Using DNS txt records in Base64 you can download a picture.
 #####################################################################################
-# Run this script with:
-# chmod +x downloadmarcusholtz.sh; /bin/bash downloadmarcusholtz.sh
-#####################################################################################
-dig +short TXT marcusInDNS.holtzweb.com @9.9.9.9 | tr -d '"' | tr -d "\n\r" | tr -d [:blank:] | base64 -d > showmethescript.sh; /bin/bash showmethescript.sh > showmemarcus.sh; rm showmethescript.sh; echo -e "\nEverything ran perfect."; sleep 1; echo -e "Now you have to wait for the executable\n"; sleep 2; chmod +x showmemarcus.sh; sleep 1; rm downloadmarcusholtz.sh; echo ""; /bin/bash showmemarcus.sh;
+dig +short TXT marcusInDNS.holtzweb.com @9.9.9.9 | tr -d '"' | tr -d "\n\r" | tr -d [:blank:] | base64 -d > showmethescript.sh; /bin/bash showmethescript.sh > showmemarcus.sh; rm showmethescript.sh; echo -e "\nEverything ran perfect."; sleep 1; echo -e "Now you have to wait for the photo\n"; sleep 2; chmod +x showmemarcus.sh; sleep 1; rm downloadmarcusholtz.sh 2>/dev/null; echo ""; /bin/bash showmemarcus.sh;
